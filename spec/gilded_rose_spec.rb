0fbc86aa3c0069b9fd640358b3aca1e26eec7595
leffen <leffen@gmail.com>
2013-09-17 20:05:22 +0200

    Added some testes

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,13 +1,99 @@
 require File.join(File.dirname(__FILE__), '../lib', 'gilded_rose')
 
+def do_update_quality(org_items)
+  items = org_items.map { |item| item.dup }
+  GildedRose.new(items).update_quality()
+  items.each_with_index { |item, idx| yield(item, org_items[idx]) }
+  items
+end
+
 describe GildedRose do
 
-  describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      items[0].name.should == "fixme"
+  context "Zero day remining item" do
+    let(:items) { [Item.new("foo", 0, 0)] }
+
+    describe "#update_quality" do
+      it "does not change the name" do
+        do_update_quality(items) do |item, old_item|
+          expect(item.name).to eq("foo")
+        end
+      end
+
+      it "does update sell_in to -1" do
+        do_update_quality(items) do |item, old_item|
+          expect(item.sell_in).to eq(-1)
+        end
+      end
     end
   end
+
+  context "Once the sell by date has passed, Quality degrades twice as fast" do
+
+    it "should lower quality to 0 after sell_item with 2 as start" do
+      do_update_quality([Item.new("foo", 0, 2)]) do |item, org_item|
+        expect(item.quality).to eq(0)
+      end
+    end
+
+    it "should lower quality to 5 after sell_item with 7 as start" do
+      do_update_quality([Item.new("foo", 0, 7)]) do |item, org_item|
+        expect(item.quality).to eq(5)
+      end
+    end
+  end
+
+  context "The Quality of an item is never negative" do
+    it "should lower quality to 0 after sell_item with 0 as start" do
+      do_update_quality([Item.new("foo", 0, 0)]) do |item, org_item|
+        expect(item.quality).to eq(0)
+      end
+    end
+  end
+
+  context "Aged Brie actually increases in Quality the older it gets" do
+    let(:items) { [Item.new("Aged Brie", 2, 2)] }
+
+    it "should increase quality with 1 when aging" do
+      do_update_quality(items) do |item, org_item|
+        expect(item.quality).to eq(3)
+      end
+    end
+  end
+
+  context "The Quality of an item is never more than 50" do
+    it "should increase quality with 1 when aging with 50 as max even for aged_brie" do
+      do_update_quality([Item.new("Aged Brie", 2, 50)]) do |item, org_item|
+        expect(item.quality).to eq(50)
+      end
+    end
+  end
+
+  context "'Sulfuras', being a legendary item, never has to be sold or decreases in Quality" do
+    let (:sulfuras_items){[Item.new("Sulfuras, Hand of Ragnaros", 1, 1)]}
+    it "should not decrease sell_in when updating quality" do
+      do_update_quality(sulfuras_items) do |item, org_item|
+        expect(item.sell_in).to eq(org_item.sell_in)
+      end
+    end
+
+    it "should not decrease quality when updating quality" do
+      do_update_quality(sulfuras_items) do |item, org_item|
+        expect(item.quality).to eq(org_item.quality)
+      end
+    end
+  end
+
+  context "'Backstage passes', like aged brie, increases in Quality as it's SellIn value approaches;
+          Quality increases by 2 when there are 10 days or less
+                  and by 3 when there are 5 days or less
+                  but Quality drops to 0 after the concert" do
+    let(:items_12_days) {[Item.new("Backstage passes to a TAFKAL80ETC concert",12,2)]}
+    let(:items_5_days) {[Item.new("Backstage passes to a TAFKAL80ETC concert",5,2)]}
+    let(:items_0_days) {[Item.new("Backstage passes to a TAFKAL80ETC concert",0,2)]}
+
+
+
+  end
+
 
 end
